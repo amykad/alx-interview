edcb05e9ab56ac381de65add663ba1c86015eccb
amykad <amina.kaddal.1@gmail.com>
2024-08-13 13:35:35 -0700

    update

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,74 +1,92 @@
 #!/usr/bin/python3
+
 """
-Log stats module
+Write a script that reads stdin line by line and computes metrics:
+
+Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1"
+ <status code> <file size> (if the format is not this one, the
+ 178.10.226.186 - [2023-12-21 21:58:06.848107] "GET /projects/260
+   HTTP/1.1" 405 33
+ line must be skipped)
+After every 10 lines and/or a keyboard interruption (CTRL + C),
+print these statistics from the beginning:
+    Total file size: File size: <total size>
+    where <total size> is the sum of all previous <file size>
+    (see input format above)
+    Number of lines by status code:
+        possible status code: 200, 301, 400, 401, 403, 404, 405
+          and 500
+        if a status code doesn’t appear or is not an integer, don’t
+          print anything for this status code
+        format: <status code>: <number>
+        status codes should be printed in ascending order
+Warning: In this sample, you will have random value - it’s normal
+ to not have the same output as this one.
 """
+
 import sys
-from operator import itemgetter
 
 
-def log_parser(log):
-    """
-    Parses log into different fields
-    """
-    log_fields = log.split()
-    file_size = int(log_fields[-1])
-    status_code = log_fields[-2]
-    return status_code, file_size
+def printer(stats: dict, file_size: int) -> None:
+    """ Function to print statistics """
+    print("File size: {:d}".format(file_size))
+
+    for k, v in sorted(stats.items()):
+        if v:
+            print("{}: {}".format(k, v))
 
 
-def validate_format(log):
-    """
-    Validates log format
-    """
-    return False if len(log.split()) < 7 else True
+def function() -> None:
+    """ Stdin processing function """
+    # Initialize variables for file size and line count
+    line_count, file_size = 0, 0
+    # List of status codes to track
+    stat_codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    # Dictionary to store count for each status code
+    stat_count = {k: 0 for k in stat_codes}
 
-
-def validate_status_code(status_code):
-    """
-    Check if status code entry is valid
-    """
-    valid_status_codes = ["200", "301", "400", "401",
-                          "403", "404", "405", "500"]
-    return True if status_code in valid_status_codes else False
-
-
-def print_log(file_size, status_codes) -> None:
-    """
-    Prints out log files
-    """
-    sorted_status_codes = sorted(status_codes.items(), key=itemgetter(0))
-    print('File size: {}'.format(file_size))
-    for code_count in sorted_status_codes:
-        key = code_count[0]
-        value = code_count[1]
-        print("{}: {}".format(key, value))
-
-
-def main():
-    """
-    Reads logs from std in and prints out statistic
-    on status code and file size
-    """
-    status_codes_count = {}
-    total_size = 0
-    log_count = 0
     try:
-        for log in sys.stdin:
-            log_count += 1
-            if not validate_format(log):
-                continue
-            status_code, file_size = log_parser(log)
-            total_size += file_size
-            if validate_status_code(status_code):
-                entry = {status_code:
-                         status_codes_count.get(status_code, 0) + 1}
-                status_codes_count.update(entry)
-            if not log_count % 10:
-                print_log(total_size, status_codes_count)
+        # Iterate through each line from stdin
+        for line in sys.stdin:
+            # Increment line count
+            line_count += 1
+
+            # Split the line by spaces into a list of words
+            data = line.split()
+            # print(data)
+
+            try:
+                # Attempt to extract the status code from the line
+                # Position; before last
+                status_code = data[-2]
+
+                # If the status code is in the list; update count
+                if status_code in stat_count:
+                    stat_count[status_code] += 1
+            except Exception:
+                pass
+
+            try:
+                # Attempt to extract the file size from the line and
+                # update total file size
+                file_size += int(data[-1])
+            except Exception:
+                pass
+
+            # Print statistics every 10 lines
+            if line_count % 10 == 0:
+                # calling printer
+                printer(stat_count, file_size)
+
+        # Print final statistics
+        printer(stat_count, file_size)
+
     except KeyboardInterrupt:
-        print_log(total_size, status_codes_count)
-    print_log(total_size, status_codes_count)
+        # Handle keyboard interruption (Ctrl+C) and print final statistics
+        printer(stat_count, file_size)
+        raise
 
 
 if __name__ == '__main__':
-    main()
+    """ Import control """
+    function())
